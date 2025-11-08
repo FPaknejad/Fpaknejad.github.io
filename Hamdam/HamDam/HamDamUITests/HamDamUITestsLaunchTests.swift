a76e082e667ac62f918b4fbdaa48aa21d17d2316
Donya <ftpaknejad@gmail.com>
2025-11-08 14:45:33 +0100

    nothing yet

---
+++ Hamdam/HamDam/HamDamUITests/HamDamUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  HamDamUITestsLaunchTests.swift
+//  HamDamUITests
+//
+//  Created by Fatemeh Paknejad on 03.11.25.
+//
+
+import XCTest
+
+final class HamDamUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
